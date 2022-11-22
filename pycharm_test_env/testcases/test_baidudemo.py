fd079d68e2e3f925f94e26eacc75e867b90c24a5
fengsonglin <fengsonglin@hltn.com>
2022-11-22 21:48:13 +0800

    feat: 百度自动搜索初探

--- pycharm_test_env/testcases/test_baidudemo.py
+++ pycharm_test_env/testcases/test_baidudemo.py
@@ -7,21 +7,23 @@
 import allure
 import pytest
 from selenium import  webdriver
-import  time
+import time
+from selenium.webdriver.common.by import By
 
 @allure.testcase("http://www.github.com")
 @allure.feature("百度搜索")
 @pytest.mark.parametrize('test_data1',['allure','pytest','unittest'])
 def test_steps_demo(test_data1):
         with allure.step("打开百度网页"):
-            driver = webdriver.Chrome("/Users/fengsonglin/project/TestTech/pycharm_test_env/testcases/resources")
+            # "/Users/fengsonglin/project/TestTech/pycharm_test_env/testcases/chromedriver.exe"
+            driver = webdriver.Chrome()
             driver.get("http://www.baidu.com")
             driver.maximize_window()
 
         with allure.step(f"输入搜索词:{test_data1}"):
-            driver.find_element("kw").send_keys(test_data1)
+            driver.find_element(by=By.ID, value='kw').send_keys(test_data1)
             time.sleep(2)
-            driver.find_element("su").click()
+            driver.find_element(by=By.ID, value='su').click()
             time.sleep(2)
 
         with allure.step("保存图片"):
@@ -31,6 +33,7 @@ def test_steps_demo(test_data1):
         with allure.step("退出浏览器"):
             driver.quit()
 
-
+if __name__ == '__main__':
+    pytest.main()
 
 
