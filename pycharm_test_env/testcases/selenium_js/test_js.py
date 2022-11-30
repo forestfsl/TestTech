93da25bfe09e2ef0007a20a35c0f4b5bc27e7123
fengsonglin <fengsonglin@hltn.com>
2022-11-30 22:28:43 +0800

    feat: js 更改日期

--- pycharm_test_env/testcases/selenium_js/test_js.py
+++ pycharm_test_env/testcases/selenium_js/test_js.py
@@ -6,12 +6,14 @@
 """
 import time
 
+import pytest
 from selenium.webdriver.common.by import By
 
 from testcases.selenium_js.base import Base
 
 
 class TestJS(Base):
+    @pytest.mark.skip
     def test_js_scroll(self):
         self.driver.get("http://www.baidu.com")
         self.driver.find_element(by=By.ID,value="kw").send_keys("selenium测试")
@@ -28,3 +30,21 @@ class TestJS(Base):
             'return document.title','return JSON.stringify(performance.timing)'
         ]:
             print(self.driver.execute_script(code))
+
+    """
+    打开网址https://www.12306.cn/index/
+    修改出发日期为2022-01-01
+    打印出发日期
+    关闭网址
+    """
+    def test_js_datetime(self):
+        self.driver.get("https://www.12306.cn/index/")
+        #用js去掉readonly属性，然后直接输入日期文本
+        date_js="document.getElementById('train_date').removeAttribute('readonly')"
+        self.driver.execute_script(date_js)
+        time.sleep(2)
+        date_js_1 = "document.getElementById('train_date').value='2023-01-01'"
+        self.driver.execute_script(date_js_1)
+        print("打印")
+        print('f当前打印日志:{self.driver.find_element(by=By.ID,value="train_date")')
+        time.sleep(5)
