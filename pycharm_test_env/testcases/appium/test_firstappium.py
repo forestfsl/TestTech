a9dabc896e4817f96fe8a6e8d594426311892341
fengsonglin <fengsonglin@hltn.com>
2022-12-05 00:38:23 +0800

    feat: 添加测试脚本

--- pycharm_test_env/testcases/appium/test_firstappium.py
+++ pycharm_test_env/testcases/appium/test_firstappium.py
@@ -4,6 +4,7 @@
 @author:songlin
 @time: 2022/12/5 00:09
 """
+import pytest
 from appium import webdriver
 """
 [0:14:23][~]$ adb devices
@@ -12,11 +13,21 @@ from appium import webdriver
 List of devices attached
 emulator-5554	offline
 """
-desired_caps={}
-desired_caps['platformName']='Android'
-desired_caps['platformVersion']='6.0'
-desired_caps['deviceName']='emulator-5554'
-desired_caps['appPackage']='com.android.settings'
-desired_caps['appActivity']='com.android.settings.Settings'
-driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
-driver.quit()
+
+class TestAppium:
+
+    def test_firstappium_demo(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '6.0'
+        desired_caps['deviceName'] = 'emulator-5554'
+        desired_caps['appPackage'] = 'com.android.settings'
+        desired_caps['appActivity'] = 'com.android.settings.Settings'
+        driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        driver.quit()
+
+if __name__ == '__main__':
+    pytest.main()
+
+
+
