690c2f00cc2ffef62fa0f935e1211eb8f45fa5bb
fengsonglin <fengsonglin@hltn.com>
2022-12-10 22:24:47 +0800

    feat: app控件交互

--- pycharm_test_env/testcases/appium/test_dwpytest.py
+++ pycharm_test_env/testcases/appium/test_dwpytest.py
@@ -31,6 +31,7 @@ class TestDw():
         self.driver.back()
         self.driver.quit()
 
+    @pytest.mark.skip
     def test_search(self):
         print("搜索测试用例")
         """
@@ -51,9 +52,40 @@ class TestDw():
         """
         current_price = float(self.driver.find_element(by=By.ID,value="com.xueqiu.android:id/current_price_dtv").text)
         assert current_price > 90
-
-
         sleep(3)
+
+    def test_attr(self):
+        """
+        打开雪球应用首页
+        定位首页的搜索框
+        判断搜索框是否可用，并查看搜索框name属性值
+        打印搜索框这个元素的左上角坐标和它的宽高
+        向搜索框输入:alibaba
+        判断【阿里巴巴】是否课件
+        如果课件 打印搜索成功，如果不可见，打印搜索失败
+        appium-uiautomator2-server 源码
+        :return:
+        """
+        element = self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/home_search")
+        search_enabled = element.is_enabled()
+        print(element.text)
+        print(element.location)
+        print(element.size)
+        if search_enabled == True:
+            element.click()
+            self.driver.find_element(by=By.ID, value="com.xueqiu.android:id/search_input_text").send_keys("alibaba")
+            alibaba_element = self.driver.find_element(by=By.XPATH, value="//*[@resource-id='com.xueqiu.android:id/name' and @text='阿里巴巴']")
+            print(alibaba_element.get_attribute("displayed"))
+            element_display = alibaba_element.get_attribute("displayed")
+            if element_display == 'true':
+                print("搜索成功")
+            else:
+                print("搜索失败")
+
+
+
+
+
 
 if __name__ == '__main__':
     pytest.main()
